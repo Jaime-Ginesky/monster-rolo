b525b35cadbd1880150a55766ffe198f29877b24
Jaime-Ginesky <jaimelee79@gmail.com>
2023-04-22 20:44:55 -0700

    added state to name button

--- src/App.js
+++ src/App.js
@@ -5,27 +5,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+ constructor() {
+   super();
 
+    this.state = {
+      name: {firstName: 'Jaime', lastName: 'Ginesky'}
+    };
+ }
   render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {this.state.name.firstName} {this.state.name.lastName}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button onClick={() => {
+        this.setState(
+          () => {
+          return { 
+            name: {firstName: 'Bonnie', lastName: 'Ex'},
+          };
+        },
+        () => {
+        console.log(this.state);
+       }
+  );
+       }}
+       >
+        Change Name
+       </button>
       </header>
     </div>
   );
 }
 }
+
 
 export default App;
