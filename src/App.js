e888262842e93ea55c287cbc775f044c888d9853
Jaime-Ginesky <jaimelee79@gmail.com>
2023-04-24 22:15:30 -0700

    added search box and filter

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
  }
 
@@ -25,9 +26,26 @@ class App extends Component {
 }
 
   render() {
-  return (
+   
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+    return (
     <div className="App">
-       {this.state.monsters.map((monster) => {
+      <input 
+      className='search-box' 
+      type='search' 
+      placeholder='search monsters' 
+      onChange={(event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+       
+        
+        this.setState(() => {
+          return { searchField };
+        });
+      }} />
+       { filteredMonsters.map((monster) => {
         return <div key={monster.id}>
           <h1>{monster.name}</h1>
           </div>
