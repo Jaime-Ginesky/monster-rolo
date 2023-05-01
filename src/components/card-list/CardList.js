5ff531e8531cfadcc4b3baf823de639eb314dbe5
Jaime-Ginesky <jaimelee79@gmail.com>
2023-04-30 22:39:29 -0700

    converted card-list,card-container and searchbox components to functional cts

--- src/components/card-list/CardList.js
+++ src/components/card-list/CardList.js
@@ -1,24 +1,18 @@
-import { Component } from 'react';
+
 
 import './card-list.styles.css';
 import CardContainer from '../card-container/CardContainer';
 
-class CardList extends Component {
-    render() {
-      const { monsters } = this.props;
+const CardList = ({ monsters }) => {
        
-      return (
         // Card image, name and email
         <div className='card-list'>
            {monsters.map((monster) => {
-        
             return (
             <CardContainer monster={monster} />
             );
        })}
     </div>   
-   )
   }
-}
 
 export default CardList;
